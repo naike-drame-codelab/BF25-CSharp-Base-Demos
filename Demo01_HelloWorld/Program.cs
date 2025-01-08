c0ec5b029cc8dad737aee7f4c5253abb81eff559
Naïké Dramé <naike.drame.dev@gmail.com>
2025-01-08 10:46:38 +0100

    added demo console

---
+++ Demo01_HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Text;
+
+#region Console.WriteLine(), Console.ReadLine()
+
+// Ecrire dans la console
+Console.WriteLine("Bonjour!");
+Console.WriteLine("Comment vous appelez-vous ?");
+
+// Lire dans la console
+string input = Console.ReadLine();
+
+ConsoleKey key = Console.ReadKey(true).Key;
+
+if (key == ConsoleKey.Enter)
+{
+    Console.WriteLine("Fais qqch");
+}
+else
+{
+    Console.WriteLine("Fais autre chose");
+}
+
+
+Console.WriteLine("Bienvenue " + input + " ! ♥");
+
+
+#endregion
