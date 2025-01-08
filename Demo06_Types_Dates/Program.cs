e678076626fb6ac0c67ea16adc674dec29d96073
Naïké Dramé <naike.drame.dev@gmail.com>
2025-01-08 14:59:33 +0100

    added demo types dates

---
+++ Demo06_Types_Dates/Program.cs
@@ -0,0 +1,29 @@
+﻿#region Types DateTime
+
+// créer une date
+DateTime date = new DateTime();
+Console.WriteLine(date);
+
+// créer une date basée sur la date du jour
+DateTime today = DateTime.Now;
+
+// formater une date
+Console.WriteLine(today.ToString("dd/MM/yyyy"));
+Console.WriteLine($"{today:dd/MM/yyyy}");
+
+// créer une date précise
+// DateTime startTraining = new DateTime(2024, 12, 9);
+DateTime startTraining = DateTime.Parse("2024-12-09");
+Console.WriteLine(startTraining);
+
+Console.WriteLine(startTraining.Year);
+Console.WriteLine(startTraining.DayOfWeek);
+
+// calculer la différence entre 2 dates
+DateTime birthday = new DateTime(1990, 9, 20);
+Console.WriteLine((today - birthday).TotalDays);
+
+Console.ReadKey();
+
+
+#endregion
