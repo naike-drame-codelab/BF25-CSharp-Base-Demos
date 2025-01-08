76fe535c5bcb303e3de426190eaf9a301c3419ac
Naïké Dramé <naike.drame.dev@gmail.com>
2025-01-08 12:56:16 +0100

    added demo types num

--- Demo03_Swap/Program.cs
+++ Demo03_Swap/Program.cs
@@ -12,8 +12,8 @@ v2 = t;
 // inverser 2 variables via un tuple
 (v1, v2) = (v2, v1);
 
-Console.WriteLine("V1:" + v1);
-Console.WriteLine("V2:" + v2);
+Console.WriteLine("V1 : " + v1);
+Console.WriteLine("V2 : " + v2);
 
 
 #endregion
---
+++ Demo04_Types_Numeriques/Program.cs
@@ -0,0 +1,31 @@
+﻿
+#region Types numériques
+
+// int
+int age = 42;
+int ageInBinary = 0b101010;
+int ageInHexa = 0x2A;
+
+Console.WriteLine(age);
+Console.WriteLine(ageInBinary);
+Console.WriteLine(ageInHexa);
+
+// long
+long l = 2_000_000_000L * 10;
+
+double value = 3D / 2;
+Console.WriteLine(value);
+
+// decimal
+Console.WriteLine(0.1M * 3);
+Console.WriteLine(1M / 3 * 3);
+
+// float
+Console.WriteLine(0.1F * 3);
+Console.WriteLine(1F / 3 * 3);
+
+// petits nombres
+short val = 16;
+byte val2 = 8;
+
+#endregion
